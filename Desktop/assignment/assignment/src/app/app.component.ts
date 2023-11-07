fb0513fc3347471ac12469de8593416586ab5427
Vidya022 <vidya.sadum@spectraforce.com>
2023-11-07 17:24:23 +0530

    modified css

--- Desktop/assignment/assignment/src/app/app.component.ts
+++ Desktop/assignment/assignment/src/app/app.component.ts
@@ -1,35 +1,6 @@
-// import { Component } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { AssignmentData } from './data';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-  
-//   title = 'assignment';
-//   constructor(private assignment:AssignmentData){}
-//   myformData = this.assignment.mydata;
-  
-//   ngOnInit() {
-//     console.log(this.myformData);
-//   }
-//   submitForm() {
-//     console.log('Form submitted with data:', this.myformData);
-//   }
-//   logOut() {
-//     console.log('Logging out...');
-//   }
-
-  
-// }
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AssignmentData } from './data';
-import { AbstractControl, ValidatorFn } from '@angular/forms';
-
 
 @Component({
   selector: 'app-root',
@@ -50,26 +21,8 @@ export class AppComponent {
   
   ngOnInit():void{
     console.log(this.myformData);
-    // for (let i = 0; i < this.myformData[0].formFields.fields.length; i++) {
-    //   // console.log(this.recruiterformData)
-    //   const question = this.myformData.[i];
-    //   const controlName = `question_${i}`;
-  
-    //   if(!question.branching){
-    //     if (question.qtype === 'dropdown') {
-    //       this.myForm.addControl(controlName, this.formBuilder.control('', Validators.required));
-    //     } else if (question.qtype === 'Text') {
-    //       this.myForm.addControl(controlName, this.formBuilder.control('', Validators.required));
-    //     } else if (question.qtype === 'Date') {
-    //       this.myForm.addControl(controlName, this.formBuilder.control('', Validators.required));
-    //     }
-    //   }
-  
-      
-    // }
-   
       const formControls:any = {};
-  
+    //checking the validations for the fields
       for (const field of this.myformData[0].formFields) {
         for (let index = 0; index < field.fields.length; index++) {
           const question = field.fields[index];
@@ -97,18 +50,6 @@ export class AppComponent {
         }
        
       }
-     
-      
-      
-      // const myForm: FormGroup = this.fb.group(formControls);
-  
-  
-    // Create a function to build the form with generated controls
-    // buildForm() {
-    //   const formControls = this.generateFormControlsAndValidations();
-    //   const myForm: FormGroup = this.fb.group(formControls);
-    //   return myForm;
-    // }
   }
 
   onSubmit() {
